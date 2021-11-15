b16cfb452d8f3c385f19a9999de0d689c5aff5ef
jorgepreis <jorgeheaton4@gmail.com>
2021-11-15 16:20:27 +0000

    1.a) done (improvement possible)

--- TP1/ex1.py
+++ TP1/ex1.py
@@ -1,4 +1,18 @@
+import math
 import numpy as np
+
+
+def most_frequent(List):
+    counter = 0
+    num = List[0]
+
+    for i in List:
+        curr_frequency = List.count(i)
+        if (curr_frequency > counter):
+            counter = curr_frequency
+            num = i
+
+    return num
 
 exit = False
 while not exit:
@@ -11,20 +25,22 @@ while not exit:
     else:
         entropy = 0
         f = open(filename, "r")
-        words = f.read().split()
+        words = f.read()
+        #words_as_bytes = str.encode(words)
         f.close()
-        words = [int(x) for x in words]
+        words = [bytes(x, 'utf8') for x in words] # encoding characters to bytes
         unique = []
         for x in words:
             if x not in unique:
                 unique.append(x)
             
-        wordsList = np.tolist(words)
+        wordsList = list(words)
         
         for y in unique:
             numberOfOcc = wordsList.count(y)
-            entropy += (numberOfOcc/256) * math.log(1/(numberOfOcc/256),2)
-            
+            entropy += (numberOfOcc/256) * math.log(1/(numberOfOcc/256), 2) # entropy calculation
+        # FINAL RESULT
         print(entropy)
+        print(most_frequent(wordsList))
             
             
