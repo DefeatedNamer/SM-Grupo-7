544d7a17f0ee95f855f0bc362b41972233bf9aba
DefeatedNamer <55286211+DefeatedNamer@users.noreply.github.com>
2021-11-26 19:08:30 +0000

    1.a fix

--- TP1/ex1.py
+++ TP1/ex1.py
@@ -10,23 +10,28 @@ def ex1():
 
         else:
             entropy = 0
-            bytes = []
+            n_bytes = 0
+            byte_sequence = []
             occurrences = [0] * 256
 
             file = open(filename, "rb")
 
             byte = file.read(1)
             while byte:
-                bytes.append(byte)
+                byte_sequence.append(byte)
                 occurrences[int.from_bytes(byte, 'big')] += 1
+                n_bytes += 1
                 byte = file.read(1)
 
             file.close()
-
+            
             for y in occurrences:
-                if y != 0:
-                    entropy += (y / 256) * math.log(1 / (y / 256), 2)
+                p = y / n_bytes
 
-            print(bytes)
+                if p > 0:
+                    entropy -= p * math.log(p, 2)
+
+            print(byte_sequence)
             print(occurrences)
+            print(n_bytes)
             print(entropy)
