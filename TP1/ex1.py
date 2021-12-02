4c9d910abf5067ce3d68e7a42a4ca6685a8a35b6
DefeatedNamer <pedrojbspires@gmail.com>
2021-12-02 13:58:10 +0000

    ex 1 and 2

--- TP1/ex1.py
+++ TP1/ex1.py
@@ -8,43 +8,56 @@ def ex1():
         if filename.lower() == "exit":
             break
 
-        else:
-            n_bytes = 0
-            byte_sequence = []
-
-            symbol_occurrences = [0] * 256
-            symbol_probability = [0.0] * 256
-
+        try:
             file = open(filename, "rb")
 
-            byte = file.read(1)
-            while byte:
-                byte_sequence.append(byte)
-                symbol_occurrences[int.from_bytes(byte, 'big')] += 1
-                n_bytes += 1
-                byte = file.read(1)
+        except FileNotFoundError:
+            print("Wrong file or file path\n")
+            continue
 
-            file.close()
+        # Declare Variables
+        n_bytes = 0
+        symbol_sequence = []
 
-            # Calculate 1st order entropy
-            entropy1o = 0
+        symbol_occurrences = [0] * 256
+        symbol_probability = [0.0] * 256
 
-            for idx, count in enumerate(symbol_occurrences):
-                p = count / n_bytes
+        # Read file
+        symbol = file.read(1)
+        while symbol:
+            symbol = int.from_bytes(symbol, 'big')
+            n_bytes += 1
+
+            symbol_sequence.append(symbol)
+            symbol_occurrences[symbol] += 1
+
+            symbol = file.read(1)
+
+        file.close()
+
+        # Calculate 1st order entropy
+        entropy1o = 0
+
+        for idx, count in enumerate(symbol_occurrences):
+            p = count / n_bytes
+
+            if p > 0:
                 symbol_probability[idx] = p
+                entropy1o -= p * math.log(p, 2)
 
-                if p > 0:
-                    entropy1o -= p * math.log(p, 2)
+        # Calculate 2nd order entropy
+        entropy2o = 0
 
-            # pair symbols with preceding symbols
-            n_pairs = 0
-            pairs = []
+        n_pairs = 0
+        pairs = []
 
-            pair_occurrences = {}
+        pair_occurrences = {}
+        pair_probability = {}
 
-            for idx, byte in enumerate(byte_sequence):
-                if idx < len(byte_sequence) - 1:
-                    pair = (byte, byte_sequence[idx + 1])
+        for idx, symbol in enumerate(symbol_sequence):
+            if idx % 2 == 0:
+                if idx < len(symbol_sequence) - 1:
+                    pair = (symbol, symbol_sequence[idx + 1])
                     pairs.append(pair)
                     n_pairs += 1
 
@@ -53,48 +66,66 @@ def ex1():
                     else:
                         pair_occurrences[pair] = 1
 
-            # Calculate 2nd order entropy
-            entropy2o = 0
+        for idx, pair in enumerate(pair_occurrences):
+            p = pair_occurrences[pair] / n_pairs
 
-            for idx, pair in enumerate(pair_occurrences):
-                p = pair_occurrences[pair] / n_pairs
+            if p > 0:
+                pair_probability[idx] = p
+                entropy2o -= p * math.log(p, 2)
 
-                if p > 0:
-                    entropy2o -= p * math.log(p, 2)
+        entropy2o /= 2
 
-            entropy2o /= 2
+        # Calculate 1st order Markov model entropy
+        markov_entropy_1o = 0
 
-            # Calculate 1st order Markov model entropy
-            markov_entropy_1o = 0
+        n_transitions = 0
+        transitions = []
 
-            min_entropy = 8
-            max_entropy = 0
+        transition_occurrences = {}
+        transition_probability = {}
 
-            for pair in pair_occurrences:
-                count = symbol_occurrences[int.from_bytes(pair[0], 'big')]
+        for idx, symbol in enumerate(symbol_sequence):
+            if idx < len(symbol_sequence) - 1:
+                t = (symbol, symbol_sequence[idx + 1])
+                transitions.append(t)
+                n_transitions += 1
 
-                p = pair_occurrences[pair] / count
+                if t in transition_occurrences:
+                    transition_occurrences[t] += 1
+                else:
+                    transition_occurrences[t] = 1
 
-                if p > 0:
-                    h = -(symbol_probability[int.from_bytes(pair[0], 'big')] * p * math.log(p, 2))
-                    markov_entropy_1o += h
 
-                    if p < min_entropy:
-                        min_entropy = p
 
-                    if p > max_entropy:
-                        max_entropy = p
+        for t in transition_occurrences:
+            p = transition_occurrences[t] / symbol_occurrences[t[0]]
 
-            print(f"\nbyte_sequence: {byte_sequence}\n")
-            print(f"pairs_array: {pair_occurrences}\n")
+            if p > 0:
+                h = -(symbol_probability[t[0]] * p * math.log(p, 2))
+                markov_entropy_1o += h
 
-            print(f"symbol_occurrences: {symbol_occurrences}\n")
+                if p < min_p:
+                    min_p = p
 
-            print(f"n_bytes: {n_bytes}\n")
+                if p > max_p:
+                    max_p = p
+        min_p = 256
+        max_p = 0
 
-            print(f"entropia 1ª ordem: {entropy1o} bits/symbol\n")
-            print(f"entropia 2ª ordem: {entropy2o} bits/symbol\n")
-            print(f"entropia Markov 1ª ordem: {markov_entropy_1o} bits/symbol")
-            print(f"entropia Markov 1ª ordem min: {min_entropy} bits/symbol")
-            print(f"entropia Markov 1ª ordem max: {max_entropy} bits/symbol\n")
+        
 
+        # Print results
+        # print(f"\nsymbol_sequence: {symbol_sequence}\n")
+        # print(f"pairs_array: {pair_occurrences}\n")
+
+        # print(f"symbol_occurrences: {symbol_occurrences}\n")
+
+        print(f"n_bytes: {n_bytes}\n")
+
+        print(f"entropia 1ª ordem: {entropy1o} bits/symbol\n")
+        print(f"entropia 2ª ordem: {entropy2o} bits/symbol\n")
+
+        print(f"entropia Markov 1ª ordem: {markov_entropy_1o} bits/symbol")
+
+        print(f"entropia Markov 1ª ordem min: {min_p} bits/symbol")
+        print(f"entropia Markov 1ª ordem max: {max_p} bits/symbol\n")
