646652e2257b9e9913866063466e5a1f6e225adc
jorgepreis <jorgeheaton4@gmail.com>
2021-12-13 15:39:31 +0000

    ex6 done

--- TP1/ex6.py
+++ TP1/ex6.py
@@ -1,8 +1,8 @@
 def calculate_error(actual, expected):
-    return actual - expected
+    return abs(actual - expected)
 
 
-x = open('rfc7932.txt', "rb")
+x = open('23961-8.txt', "rb")
 x = x.read()
 
 preditor0 = 0
@@ -19,7 +19,7 @@ while index < len(x):
     index += 1
 
 index = 0
-print(preditor0)
+#print(preditor0)
 
 # preditor 1
 while index < len(x):
@@ -27,7 +27,7 @@ while index < len(x):
     index += 1
 
 index = 0
-print(preditor1)
+#print(preditor1)
 
 # preditor 2
 while index < len(x):
@@ -35,20 +35,26 @@ while index < len(x):
     index += 1
 
 index = 0
-print(preditor2)
+#print(preditor2)
 
 # preditor 3
 while index < len(x):
-    preditor3 += calculate_error(x[index], x[index-1] + x[index-2] + x[index-3])
+    preditor3 += calculate_error(x[index], x[index - 1] + x[index - 2] + x[index - 3])
     index += 1
 
 index = 0
-print(preditor3)
+#print(preditor3)
 
 # preditor 4
 while index < len(x):
-    preditor4 += calculate_error(x[index], x[index-2])
+    preditor4 += calculate_error(x[index], x[index - 2])
     index += 1
 
 index = 0
-print(preditor4)
+#print(preditor4)
+
+preditorDic = {"Preditor 0": preditor0, "Preditor 1": preditor1, "Preditor 2": preditor2, "Preditor 3": preditor3, "Preditor 4": preditor4}
+keyList = list(preditorDic.keys())
+valList = list(preditorDic.values())
+
+print("O melhor preditor é o:", keyList[valList.index(min(preditor0, preditor1, preditor2, preditor3, preditor4))])
